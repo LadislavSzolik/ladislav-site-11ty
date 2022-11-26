499394018a91e149633fd7117a46ac6016b2750d
Ladislav Szolik <szolik.ladislav@gmail.com>
2022-11-26 19:13:09 +0100

    Version 1.0 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
-module.exports = function(eleventyConfig){
-    return {
-        dir: {
-            input: "src",
-            data: "_data",
-            includes: "_includes",
-            layouts: "_layouts"
-        }
-    };
-}
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/downloads": "downloads" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon.ico": "favicon.ico" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/robots.txt": "robots.txt" });
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
+};
