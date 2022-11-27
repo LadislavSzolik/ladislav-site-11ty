99ca577a34d157ee6c5b9fcfc0851507499bd9a9
Ladislav Szolik <szolik.ladislav@gmail.com>
2022-11-27 11:36:40 +0100

    Add payments project

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,22 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/assets/images": "images" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/downloads": "downloads" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/favicon.ico": "favicon.ico" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/robots.txt": "robots.txt" });
-  return {
-    dir: {
-      input: "src",
-      data: "_data",
-      includes: "_includes",
-      layouts: "_layouts",
-    },
-  };
+    eleventyConfig.addPassthroughCopy({"src/assets/images": "images"});
+    eleventyConfig.addPassthroughCopy({"src/assets/downloads": "downloads"});
+    eleventyConfig.addPassthroughCopy({"src/assets/favicon.ico": "favicon.ico"});
+    eleventyConfig.addPassthroughCopy({"src/assets/robots.txt": "robots.txt"});
+
+    eleventyConfig.addPairedNunjucksShortcode("lessonsLearned", (text) => `
+        <div class="max-w-3xl mx-auto text-center bg-sky-100 rounded-2xl px-4 sm:px-7 py-4">
+            <h3 class="font-semibold">Lessons learned</h3>
+            <p>${text}</p>
+        </div>
+  `)
+
+    return {
+        dir: {
+            input: "src",
+            data: "_data",
+            includes: "_includes",
+            layouts: "_layouts",
+        },
+    };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{njk,md}", "./src/**/*.svg",],
+  content: ["./src/**/*.{njk,md}", "./src/**/*.svg"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
